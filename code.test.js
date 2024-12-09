1fac4ac5c3f6ef8076703321e7ee10f4a88b9d89
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-08 18:08:26 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,17 +1,53 @@
 const fs = require('fs');
 const jsc = require('jsverify');
 
-eval(fs.readFileSync('code.js')+'');
 
-const testSort =
-    jsc.forall("array nat", function(arr) {
-        var a1 = JSON.parse(JSON.stringify(arr));
-        var a2 = JSON.parse(JSON.stringify(arr));
-        return JSON.stringify(pancakeSort(a1)) ==
-            JSON.stringify(a2.sort(function(a, b)
-                { return a - b; }));
-    });
+function flip(array, n) 
+{
+    let left = 0;
+    let right = n - 1;
+    while (left < right)
+    {
+        [array[left], array[right]] = [array[right], array[left]];
+        left++;
+        right--;
+    }
+    return array;
+}
+
+
+function pancakeSort(array) 
+{
+    for (let size = array.length; size > 1; size--)
+    {
+        let minIndex = 0;
+        for (let i = 1; i < size; i++) 
+        {
+            if (array[i] < array[minIndex])
+            {
+                minIndex = i;
+            }
+        }
+        if (minIndex !== size - 1) 
+        {
+            if (minIndex > 0) 
+            {
+                flip(array, minIndex + 1);
+            }
+            flip(array, size);
+        }
+    }
+    return array;
+}
+
+
+const testSort = jsc.forall("array nat", function(arr)
+{
+    let a1 = JSON.parse(JSON.stringify(arr));
+    let a2 = JSON.parse(JSON.stringify(arr));
+    return JSON.stringify(pancakeSort(a1)) ===
+           JSON.stringify(a2.sort((a, b) => a - b));
+});
 
 jsc.assert(testSort);
 
-// test flip
