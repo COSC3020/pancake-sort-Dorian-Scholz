ba092f4cfb8dcd5bda7a76e54e51629b27dc3a59
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-08 18:29:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,40 @@
-function flip(array, n) {
+function flip(array, n) 
+{
+    
+    let left = 0;
+    let right = n - 1;
+    while (left < right) 
+    {
+        [array[left], array[right]] = [array[right], array[left]];
+        left++;
+        right--;
+    }
     return array;
 }
 
-// Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    
+    for (let size = array.length; size > 1; size--) 
+    {
+        
+        let minIndex = 0;
+        for (let i = 1; i < size; i++) 
+        {
+            if (array[i] < array[minIndex]) 
+            {
+                minIndex = i;
+            }
+        }
+
+        
+        if (minIndex !== size - 1) 
+        {
+            if (minIndex > 0) 
+            {
+                flip(array, minIndex + 1); 
+            }
+            flip(array, size); 
+        }
+    }
     return array;
 }
